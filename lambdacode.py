82dec2b9c7a1d8b44454acdbbbf5774dbbbebec2
Fernando Faria <Fernandovfaria@gmail.com>
2016-04-20 18:42:52 -0400

    Update lambdacode.py

--- lambdacode.py
+++ lambdacode.py
@@ -22,13 +22,11 @@ def lambda_handler(event, context):
     print("event.session.application.applicationId=" +
           event['session']['application']['applicationId'])
 
-    if (event['session']['application']['applicationId'] !=
-            "amzn1.echo-sdk-ams.app.b36bad7c-ffbd-492d-8725-88c71aabba91"):
+    if (event['session']['application']['applicationId'] != "amzn1.echo-sdk-ams.app.b36bad7c-ffbd-492d-8725-88c71aabba91"):
         raise ValueError("Invalid Application ID")
 
     if event['session']['new']:
-        on_session_started({'requestId': event['request']['requestId']},
-                           event['session'])
+        on_session_started({'requestId': event['request']['requestId']}, event['session'])
 
     if event['request']['type'] == "LaunchRequest":
         return on_launch(event['request'], event['session'])
@@ -41,17 +39,13 @@ def lambda_handler(event, context):
 def on_session_started(session_started_request, session):
     """ Called when the session starts """
 
-    print("on_session_started requestId=" + session_started_request['requestId']
-          + ", sessionId=" + session['sessionId'])
+    print("on_session_started requestId=" + session_started_request['requestId'] + ", sessionId=" + session['sessionId'])
 
 
 def on_launch(launch_request, session):
-    """ Called when the user launches the skill without specifying what they
-    want
-    """
+    """ Called when the user launches the skill without specifying what they want """
 
-    print("on_launch requestId=" + launch_request['requestId'] +
-          ", sessionId=" + session['sessionId'])
+    print("on_launch requestId=" + launch_request['requestId'] + ", sessionId=" + session['sessionId'])
     # Dispatch to your skill's launch
     return get_welcome_response()
 
@@ -59,8 +53,7 @@ def on_launch(launch_request, session):
 def on_intent(intent_request, session):
     """ Called when the user specifies an intent for this skill """
 
-    print("on_intent requestId=" + intent_request['requestId'] +
-          ", sessionId=" + session['sessionId'])
+    print("on_intent requestId=" + intent_request['requestId'] + ", sessionId=" + session['sessionId'])
 
     intent = intent_request['intent']
     intent_name = intent_request['intent']['name']
@@ -84,11 +77,9 @@ def on_intent(intent_request, session):
 
 def on_session_ended(session_ended_request, session):
     """ Called when the user ends the session.
-
-    Is not called when the skill returns should_end_session=true
+    Is not called when the skill returns should_end_session=true 
     """
-    print("on_session_ended requestId=" + session_ended_request['requestId'] +
-          ", sessionId=" + session['sessionId'])
+    print("on_session_ended requestId=" + session_ended_request['requestId'] + ", sessionId=" + session['sessionId'])
     # add cleanup logic here
     #handle_session_end_request()
 # --------------- Functions that control the skill's behavior ------------------
@@ -108,8 +99,7 @@ def rhyme(request):
         speech_output = speech_output + ", " + req3[q]["word"]
         q += 1
     should_end_session = False
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, None, should_end_session))
+    return build_response(session_attributes, build_speechlet_response(card_title, speech_output, None, should_end_session))
 
 def metronome(request):
     session_attributes = {}
@@ -117,8 +107,7 @@ def metronome(request):
     bpm = request['intent']['slots']['Rate']['value']
     speech_output = "<speak>" + bpm + " bpm <audio src='https://s3.amazonaws.com/echo-jam-audio-files/metronome/" + bpm + "bpm.mp3' /> </speak>"
     should_end_session = False
-    return build_response(session_attributes, build_speechlet_response_ssml(
-        card_title, speech_output, None, should_end_session))
+    return build_response(session_attributes, build_speechlet_response_ssml(card_title, speech_output, None, should_end_session))
 
 def one_chord(request):
     session_attributes = {}
@@ -126,8 +115,7 @@ def one_chord(request):
     chord = request['intent']['slots']['TheChord']['value']
     speech_output = "<speak>" + chord + " chord <audio src='https://s3.amazonaws.com/echo-jam-audio-files/chords/" + chord + " chord.mp3' /> </speak>"
     should_end_session = False
-    return build_response(session_attributes, build_speechlet_response_ssml(
-        card_title, speech_output, None, should_end_session))
+    return build_response(session_attributes, build_speechlet_response_ssml(card_title, speech_output, None, should_end_session))
 
 def chord_progression(request):
     return ""
@@ -153,37 +141,29 @@ def halp(request):
         speech_output = "You can ask for help for specific features by saying, 'Help chords, help rhyming, help metronome, or help chord progression'"
     reprompt_text = "What do you need help with?"
     should_end_session = False
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
+    return build_response(session_attributes, build_speechlet_response(card_title, speech_output, reprompt_text, should_end_session))
 
 def get_welcome_response():
-    """ If we wanted to initialize the session to have some attributes we could
-    add those here
-    """
+    """ If we wanted to initialize the session to have some attributes we could add those here """
 
     session_attributes = {}
     card_title = "Welcome"
-    speech_output = "Welcome to Echo Jam. " \
-                    "To get help, say help. "
+    speech_output = "Welcome to Echo Jam. To get help, say help. "
     # If the user either does not reply to the welcome message or says something
     # that is not understood, they will be prompted again with this text.
     reprompt_text = getHelpMessage()
     should_end_session = False
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
+    return build_response(session_attributes, build_speechlet_response(card_title, speech_output, reprompt_text, should_end_session))
 
 def getHelpMessage():
-    return "You can ask me 'Give me a metronome at blank bpm' or 'Give me words that rhyme with blank'." \
-           "You can also ask me 'Give me a chord progression in key blank' or 'Give me chord blank'."
+    return "You can ask me 'Give me a metronome at blank bpm' or 'Give me words that rhyme with blank'. You can also ask me 'Give me a chord progression in key blank' or 'Give me chord blank'."
 
 def handle_session_end_request():
     card_title = "Session Ended"
-    speech_output = "Thank you for making music with Echo Jam. " \
-                    "Have a nice day! "
+    speech_output = "Thank you for making music with Echo Jam. Have a nice day! "
     # Setting this to true ends the session and exits the skill.
     should_end_session = True
-    return build_response({}, build_speechlet_response(
-        card_title, speech_output, None, should_end_session))
+    return build_response({}, build_speechlet_response(card_title, speech_output, None, should_end_session))
 
 # --------------- Helpers that build all of the responses ----------------------
 
