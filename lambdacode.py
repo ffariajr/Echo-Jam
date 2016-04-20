cd6571f6010a42615b82367c28a18753861ac588
Fernando Faria <Fernandovfaria@gmail.com>
2016-04-20 19:51:51 -0400

    Update lambdacode.py

--- lambdacode.py
+++ lambdacode.py
@@ -133,7 +133,10 @@ def chord_progression(request):
     theprog = [0, 0, 0, 0, 0]
     for z in range(0, 4):
         theprog[z] = "'" + sssrc + "chordprogression/" + progs[str(z)][root] + " chord.mp3'"
-    speech_output = "<speak>chord progression in the key of " + rootchord + " <audio src=" + theprog[0] + " /> <audio src=" + theprog[1] + " /> <audio src=" + theprog[2] + " /> <audio src=" + theprog[3] + " /> <audio src=" + theprog[4] + " /> </speak>"
+    speech_output = "<speak>chord progression in the key of " + rootchord
+    for z in range(0, 4):
+        speech_output.append(" <audio src=" + theprog[z] + " />")
+    speech_output.append(" </speak>")
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response_ssml(card_title, speech_output, None, should_end_session))
 
