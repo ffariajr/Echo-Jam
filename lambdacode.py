da2f75aaed3e01312560d3556505b66be024eb50
Fernando Faria <Fernandovfaria@gmail.com>
2016-04-25 13:01:48 -0400

    Update lambdacode.py

--- lambdacode.py
+++ lambdacode.py
@@ -120,7 +120,7 @@ def metronome(request):
 def one_chord(request):
     session_attributes = {}
     card_title = "Chord"
-    chord = request['intent']['slots']['TheChord']['value']
+    chord = request['intent']['slots']['TheChord']['value'].replace(" ", "+")
     speech_output = "<speak>" + chord + " chord <audio src=" + sssrc + "chords/" + chord + "+chord.mp3' /> </speak>"
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response_ssml(card_title, speech_output, None, should_end_session))
