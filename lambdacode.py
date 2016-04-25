31af00366ed1aed5ff2d312bb0aec83781f77329
Fernando Faria <Fernandovfaria@gmail.com>
2016-04-25 13:00:17 -0400

    Update lambdacode.py

--- lambdacode.py
+++ lambdacode.py
@@ -121,7 +121,7 @@ def one_chord(request):
     session_attributes = {}
     card_title = "Chord"
     chord = request['intent']['slots']['TheChord']['value']
-    speech_output = "<speak>" + chord + " chord <audio src=" + sssrc + "chords/" + chord + " chord.mp3' /> </speak>"
+    speech_output = "<speak>" + chord + " chord <audio src=" + sssrc + "chords/" + chord + "+chord.mp3' /> </speak>"
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response_ssml(card_title, speech_output, None, should_end_session))
 
@@ -132,10 +132,10 @@ def chord_progression(request):
     root = prog1.index(rootchord)
     theprog = [0, 0, 0, 0, 0]
     for z in range(0, 5):
-        theprog[z] = progs[str(z)][root]
+        theprog[z] = progs[str(z)][root].replace(" ", "+")
     speech_output = "<speak> chord progression in the key of " + rootchord
     for z in range(0, 5):
-        speech_output += " <audio src='" + sssrc + "chordprogression/" + theprog[z] + " chord.mp3' />"
+        speech_output += " <audio src='" + sssrc + "chords/" + theprog[z] + "+chord.mp3' />"
     speech_output += " </speak>"
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response_ssml(card_title, speech_output, None, should_end_session))
