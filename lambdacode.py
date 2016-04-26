79ecb1a80255288a0fab4609e4fa82cdacad6068
gregheitman <heitmag1@tcnj.edu>
2016-04-26 19:14:27 -0400

    Update lambdacode.py

--- lambdacode.py
+++ lambdacode.py
@@ -121,7 +121,7 @@ def one_chord(request):
     session_attributes = {}
     card_title = "Chord"
     chord = request['intent']['slots']['TheChord']['value']
-    speech_output = "<speak>" + chord + " chord <audio src=" + sssrc + "chords/" + chord.replace(" ", "+") + "+chord.mp3' /> </speak>"
+    speech_output = "<speak>" + chord + " chord <audio src=" + sssrc + "chords/" + chord.replace(" ", "+").replace(".", "") + "+chord.mp3' /> </speak>"
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response_ssml(card_title, speech_output, None, should_end_session))
 
