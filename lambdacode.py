936123ae02f6bc5f916ea518280ea17e3dd08a54
Jonathan Arban <arbanj1@apps.tcnj.edu>
2016-04-28 14:32:53 -0400

    Update lambdacode.py

--- lambdacode.py
+++ lambdacode.py
@@ -137,6 +137,7 @@ def metronome(request):
     session_attributes = {}
     card_title = "Metronome"
     bpm = request['intent']['slots']['Rate']['value']
+    bpm -= bpm % 5
     speech_output = "<speak>" + bpm + " bpm <audio src=" + sssrc + "metronome/" + bpm + "bpm.mp3' /> </speak>"
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response_ssml(card_title, speech_output, None, should_end_session))
